a64f24d81cc981630099efbe57801be779326bd9
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-28 21:47:20 +0300

    adds home page

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,14 @@
+import './home.css'
+
+export default function Home() {
+    return (
+        <div className='header'>
+            <div className="headerTitles">
+                <span className='headerTitleSm'>React</span>
+                <span className='headerTitleLg'>Blog</span>
+            </div>
+            <img className='headerImg' src='https://images.pexels.com/photos/1072824/pexels-photo-1072824.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='' />
+            
+        </div>
+    )
+}
