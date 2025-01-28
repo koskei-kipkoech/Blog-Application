89085d980b26dd3379c533d944e887fdc28120eb
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-28 21:54:24 +0300

    adds testing links

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about/About';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
+import Navbar from './components/navbar/Navbar';
+import Postdetails from './components/postdetails/Postdetails';
+import Postform from './components/postform/Postform';
+import Postlist from './components/postlist/Postlist';
+import Register from './components/register/Register';
+import Settings from './components/settings/Setting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Postdetails/>
+  )
 }
 
 export default App;
