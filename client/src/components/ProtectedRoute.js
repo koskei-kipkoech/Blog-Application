ff9d6d574216d8fc8d92960b0a75e3bf6b45f9f0
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-30 20:01:20 +0300

    adds post details contexts

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = ({ element }) => {
+    const { user } = useAuth();
+    console.log('protectedRoute -user:', user)
+
+    return user ? element : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
