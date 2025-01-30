91397dd32b55d2e15fd5a44628ad62ed4e81a4ed
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-30 08:53:18 +0300

    adds routes and routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/about/About';
+import FavouriteLikedPost from './components/favourites/Favourite';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
 import Navbar from './components/navbar/Navbar';
@@ -8,11 +9,25 @@ import Postform from './components/postform/Postform';
 import Postlist from './components/postlist/Postlist';
 import Register from './components/register/Register';
 import Settings from './components/settings/Setting';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Postdetails/>
-  )
+    <Router>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/postdetails/:postId" element={<Postdetails />} />
+        <Route path="/postform" element={<Postform />} />
+        <Route path="/postlist" element={<Postlist />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/favliked" element={<FavouriteLikedPost />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
