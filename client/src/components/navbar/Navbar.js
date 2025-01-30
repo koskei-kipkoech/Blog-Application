ee62c567fd354820633f8b8ca9d0dbad0f00191f
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-30 08:58:47 +0300

    updates routes to different components

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,4 +1,6 @@
 import "./navbar.css"
+import { Link } from 'react-router-dom';
+
 
 export default function TopBar() {
 
@@ -12,18 +14,19 @@ export default function TopBar() {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem" >HOME</li>
-                    <li className="topListItem" >ABOUT</li>
-                    <li className="topListItem" >NEWPOST</li>
-                    <li className="topListItem" >FAVOURITEPOSTS</li>
-                    <li className="topListItem" >LOGOUT</li>
+                    <li className="topListItem" ><Link className="link" to='/'>HOME</Link></li>
+                    <li className="topListItem" ><Link className="link" to='/about'>ABOUT</Link></li>
+                    <li className="topListItem" ><Link className="link" to='/postform'>ADDPOST</Link></li>
+                    <li className="topListItem" ><Link className="link" to='/postdetails'>FAVOURITES</Link></li>
+                    <li className="topListItem" ><Link className="link" to='/favliked'>LIKED</Link></li>
+                    <li className="topListItem" ><Link className="link" to='/'>LOGOUT</Link></li>
 
                     
                 </ul>
             </div>
             <div className="topRight">
-            <i className="topSearchIcon fa-solid fa-gear"></i>
-            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+                <Link className="link" to='settings'><i className="topSearchIcon fa-solid fa-gear"></i></Link>
+                <Link className="link" to='/'><i className="topSearchIcon fa-solid fa-magnifying-glass"></i></Link>
             </div>
         </div>
     )
