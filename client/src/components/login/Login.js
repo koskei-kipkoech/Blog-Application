2d72c6b3541dde821a5620c39bcb292b3b465f29
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-30 08:59:47 +0300

    adds changes on user login

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,17 +1,40 @@
 import './login.css'
+import {useState} from 'react'
 
 export default function Login(){
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    const [errorMessage,setErrorMessage] = useState('')
+
+    const handleLogin = async(e) => {
+        e.preventDefault()
+        const response  = await  fetch('http://127.0.0.1:5555/login',{
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, password})
+        });
+        const data = await response.json();
+        if (response.status ===200){
+            alert('Login successful')
+            console.log(data.token);
+        }else{
+            setErrorMessage(data.message);
+        }
+    };
+
     return(
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
+            <form className="loginForm" onSubmit={handleLogin}>
                 <label>Username</label>
-                <input className="loginInput" type="text" placeholder="Enter your username..." />
+                <input className="loginInput" type="text" placeholder="Enter your username..." value={username} onChange={(e) => setUsername(e.target.value)}/>
                 <label>Password</label>
-                <input className="loginInput" type="password" placeholder="Enter your password..." />
+                <input className="loginInput" type="password" placeholder="Enter your password..." value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <button className="loginButton" type="submit">Login</button>
             </form>
-            
+            {errorMessage && <p className='error'>{errorMessage}</p>}
             <button className="loginRegisterButton">Register</button>
             
         </div>
