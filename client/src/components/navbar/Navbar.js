685349cd05ebda8334ea7cc74d75eca1d6b12344
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-30 19:52:02 +0300

    updates routes for auth

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,11 +1,13 @@
-import "./navbar.css"
-import { Link } from 'react-router-dom';
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
-
-export default function TopBar() {
+export default function Navbarr() {
+    const { user, logout } = useAuth();
+    console.log('Navbar = user: ', user);
 
     return (
-        <div className='top' >
+        <div className="top">
             <div className="topLeft">
                 <i className="topIcon fa-brands fa-linkedin"></i>
                 <i className="topIcon fa-brands fa-github"></i>
@@ -14,21 +16,34 @@ export default function TopBar() {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem" ><Link className="link" to='/'>HOME</Link></li>
-                    <li className="topListItem" ><Link className="link" to='/about'>ABOUT</Link></li>
-                    <li className="topListItem" ><Link className="link" to='/postform'>ADDPOST</Link></li>
-                    <li className="topListItem" ><Link className="link" to='/postdetails'>FAVOURITES</Link></li>
-                    <li className="topListItem" ><Link className="link" to='/favliked'>LIKED</Link></li>
-                    <li className="topListItem" ><Link className="link" to='/'>LOGOUT</Link></li>
-
-                    
+                    <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
+                    <li className="topListItem"><Link className="link" to="/about">ABOUT</Link></li>
+                    {user ? (
+                        <>
+                            <li className="topListItem"><Link className="link" to="/postform">ADDPOST</Link></li>
+                            <li className="topListItem"><Link className="link" to="/postdetails">FAVOURITES</Link></li>
+                            <li className="topListItem"><Link className="link" to="/favliked">FAV/LIKED</Link></li>
+                            <li className="topListItem" onClick={logout} style={{ cursor: "pointer" }}>LOGOUT</li>
+                        </>
+                    ) : (
+                        <li className="topListItem"><Link className="link" to="/login">LOGIN</Link></li>
+                    )}
                 </ul>
             </div>
             <div className="topRight">
-                <Link className="link" to='settings'><i className="topSearchIcon fa-solid fa-gear"></i></Link>
-                <Link className="link" to='/'><i className="topSearchIcon fa-solid fa-magnifying-glass"></i></Link>
-                <Link className="link" id='login' to='/login'>Login</Link>
+                {user ? (
+                    <>
+                        <Link className="link" to="/settings">
+                            <i className="topSearchIcon fa-solid fa-gear"></i>
+                        </Link>
+                        <Link className="link" to="/">
+                            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+                        </Link>
+                    </>
+                ) : (
+                    <Link className="link" to="/login" id="login">Login</Link>
+                )}
             </div>
         </div>
-    )
+    );
 }
