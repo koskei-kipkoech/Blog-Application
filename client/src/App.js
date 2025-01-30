e547e4ba19dfb66e9d77b411d8a46194bc35334d
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-30 19:56:30 +0300

    updates authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,38 @@
-import './App.css';
-import About from './components/about/About';
-import FavouriteLikedPost from './components/favourites/Favourite';
-import Home from './components/home/Home';
-import Login from './components/login/Login';
-import Navbar from './components/navbar/Navbar';
-import Postdetails from './components/postdetails/Postdetails';
-import Postform from './components/postform/Postform';
-import Postlist from './components/postlist/Postlist';
-import Register from './components/register/Register';
-import Settings from './components/settings/Setting';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import About from "./components/about/About";
+import FavouriteLikedPost from "./components/favourites/Favourite";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Navbar from "./components/navbar/Navbar";
+import Postdetails from "./components/postdetails/Postdetails";
+import Postform from "./components/postform/Postform";
+import Postlist from "./components/postlist/Postlist";
+import Register from "./components/register/Register";
+import Settings from "./components/settings/Setting";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
+import ProtectedRoute from "./components/ProtectedRoute"; // Import ProtectedRoute
 
 function App() {
   return (
-    <Router>
-      <Navbar /> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/postdetails/:postId" element={<Postdetails />} />
-        <Route path="/postform" element={<Postform />} />
-        <Route path="/postlist" element={<Postlist />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/favliked" element={<FavouriteLikedPost />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* Protect these routes, only accessible if logged in */}
+          <Route path="/postdetails/:postId" element={<ProtectedRoute element={<Postdetails />} />} />
+          <Route path="/postform" element={<ProtectedRoute element={<Postform />} />} />
+          <Route path="/postlist" element={<ProtectedRoute element={<Postlist />} />} />
+          <Route path="/settings" element={<ProtectedRoute element={<Settings />} />} />
+          <Route path="/favliked" element={<ProtectedRoute element={<FavouriteLikedPost />} />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
